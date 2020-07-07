1eebf0dae8a9ffd4f01b8df5542afa718e994884
escleonh <hescalante@atu.edu>
2020-07-07 18:06:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const modalTitle = document.querySelector('.modalTitle');
+const parks = document.getElementById('parks');
+const parkFullName = document.getElementById('parkFullName');
+const description = document.getElementById('description');
+const parkLocation = document.getElementById('location');
+
+
+//When a user click on a state, this functions triggers
+function onClick(){
+    
+   
+    let stateCode = event.target.id;
+    let stateName = event.target.getAttribute('title');
+   
+
+    modal.style.display = "block";
+        // modal.classList.add('show');
+        // modal.classList.remove('hide');
+
+        modalTitle.innerHTML= "<h2>Parks in "+stateName+"</h2>";
+
+    fetch('https://developer.nps.gov/api/v1/parks?statecode='+stateCode+'&api_key=ymoGeFH1NkQWm8tw6p1bSgpIETUX31jXzJNDMwIo')
+    .then(res => res.json() )
+    .then(data => data.data.forEach(element => {
+        
+        parks.insertAdjacentHTML("beforeend", 
+        "<div class='park'>"+ 
+            "<img src='"+element.images[0].url+"' width='300' height='200'>"+
+            "<p>"+ element.fullName+"</p>"+
+            "<p>"+ element.addresses[0].city+", "+element.addresses[0].stateCode+"</p>"+
+            "<p>"+ element.description +"</p>"+
+            "<a href='"+element.url+"' id='learnMoreBtn'>Learn More</a>"+
+        "</div>");
+
+    }));
+
+    closeBtn.addEventListener("click", function(){
+        // modal.classList.remove('show');
+        // modal.classList.add('hide');
+        modal.style.display = "none";
+        parks.innerHTML = "";
+        });
+}
+
