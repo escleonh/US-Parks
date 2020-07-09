7241782254b6c880b523f308b4ae1b415a712f02
escleonh <hescalante@atu.edu>
2020-07-08 20:13:07 -0500

    added ability to close modal by clicking outside the box and made it mobile friendly

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 const modal = document.querySelector('.modal');
-const closeBtn = document.querySelector('.close');
+const closeBtn = document.querySelector('.closeBtn');
 const modalTitle = document.querySelector('.modalTitle');
-const parks = document.getElementById('parks');
+const parksWrapper = document.getElementById('parksWrapper');
 const parkFullName = document.getElementById('parkFullName');
 const description = document.getElementById('description');
 const parkLocation = document.getElementById('location');
 const modalContent = document.querySelector('.modalContent');
 const loadingImg = document.querySelector('.loadingGIF');
+
 //When a user click on a state, this functions triggers
 function onClick(){
     
    
     let stateCode = event.target.id;
     let stateName = event.target.getAttribute('title');
-   
-    loadingImg.style.display="grid";
-    
+       
+    loadingImg.style.display="flex";
     modal.style.display = "block";
 
-        // modal.classList.add('show');
-        // modal.classList.remove('hide');
 
-        modalTitle.innerHTML= "<h2>Parks in "+stateName+"</h2>";
+    modalTitle.innerHTML= "<h2>Parks in "+stateName+"</h2>";
 
     fetch('https://developer.nps.gov/api/v1/parks?statecode='+stateCode+'&api_key=ymoGeFH1NkQWm8tw6p1bSgpIETUX31jXzJNDMwIo')
     .then(res => res.json() )
     .then(data => data.data.forEach(element => {
+
         loadingImg.style.display="none";
-        parks.insertAdjacentHTML("beforeend", 
-        "<div class='park'>"+ 
+        parksContainer.insertAdjacentHTML("beforeend", 
+        "<div class='parkWrapper'>"+ 
             "<img src='"+element.images[0].url+"' width='300' height='200'>"+
             "<p>"+ element.fullName+"</p>"+
             "<p>"+ element.addresses[0].city+", "+element.addresses[0].stateCode+"</p>"+
@@ -39,11 +38,20 @@ function onClick(){
     }));
 
     closeBtn.addEventListener("click", function(){
-        // modal.classList.remove('show');
-        // modal.classList.add('hide');
+        if(parksContainer.innerHTML != ""){
         modal.style.display = "none";
-        parks.innerHTML = "";
+        parksContainer.innerHTML = "";
+        }
 
-        });
+    });
 }
 
+function closeModal(){
+  if(event.target.getAttribute("class")=='modal'){
+    if(parksContainer.innerHTML != ""){
+    modal.style.display = "none";
+    parksContainer.innerHTML = "";
+    }
+  }
+  
+}
